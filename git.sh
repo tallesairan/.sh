9b4a29ce5d39e72896f00a474d845655800b527d
vigkk <vigkk@localhost.localdomain>
2015-10-13 21:31:52 -0300

    Tue 13 Oct 21:31:52 BRT 2015

--- git.sh
+++ git.sh
@@ -2,5 +2,17 @@
 
 cd ~/.sh
 echo $(pwd)
-git statuslls
+git=$(git status)
+ok="On branch master
+Your branch is up-to-date with 'origin/master'.
+nothing to commit, working directory clean"
+
+if [ "$git" == "$ok" ]; then
+    echo ok
+else
+    echo not ok
+    # git add *
+    # git commit -m \" "$(date)" \"
+    # git push
+fi
 
