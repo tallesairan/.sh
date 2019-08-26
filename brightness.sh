a2f21ff1a1c934d9a8b04d29b73b8cffbe3d755e
Laura Viglioni <viglionigr@gmail.com>
2019-08-26 11:20:44 -0300

    added brightness script

---
+++ brightness.sh
@@ -0,0 +1,38 @@
+monitor=$(xrandr | grep " connected" | cut -f1 -d " ")
+option=$1
+
+main(){
+    if [ "$option" = "decrease" ]; then decrease
+    elif [ "$option" = "increase" ]; then increase
+    elif [ "$option" = "get" ]; then get
+    else
+	echo "Error! Must pass arg increase/decrease/get"
+    fi
+}
+
+decrease(){
+    new_val=$(xrandr --verbose | awk '/Brightness/ { print $2 - 0.1; exit }')
+    if [ "$new_val" = "0.1" ];
+    then
+	new_val=0.2
+    fi
+    xrandr --output $monitor --brightness $new_val
+    echo "Brightness level is $new_val"
+}
+
+increase(){
+    new_val=$(xrandr --verbose | awk '/Brightness/ { print $2 + 0.1; exit }')
+    if [ "$new_val" = "1.1" ];
+    then
+	new_val=1.0
+    fi
+    xrandr --output $monitor --brightness $new_val
+    echo "Brightness level is $new_val"
+}
+
+get(){
+    echo $(xrandr --verbose | awk '/Brightness/ { print $2; exit }')
+}
+
+
+main
